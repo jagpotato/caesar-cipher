fd0e47a238a5989ca19eeab9daa5b4391803e033
daiki <dik.piyo.piyo@gmail.com>
2018-02-04 01:08:44 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const zCODE = 122
+const ZCODE = 90;
+const string = "aQke3~1eJPBg{9aZT";
+const shift = 3;
+
+console.log(string);
+caesar(string, shift);
+
+function caesar(string, shift) {
+  let splitString = string.split("");
+  let result = "";
+  splitString.map(function(char) {
+    let code = char.charCodeAt(0);
+    code += shift;
+    if ( /[a-z]/.test(char) === true ) {
+      if ( code > zCODE ) {
+        code -= 26;
+      }
+      result += String.fromCharCode(code);
+    } else if ( /[A-Z]/.test(char) === true ) {
+      if ( code > ZCODE ) {
+        code -= 26;
+      }
+      result += String.fromCharCode(code);
+    } else {
+      result += char;
+    }
+  });
+  console.log(result);
+}
